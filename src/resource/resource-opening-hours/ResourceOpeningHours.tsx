c1d936f718a8f4b9fb880ac29d90d4dfe82ac7ec
Mika Nevalainen <mn.nevalainen@gmail.com>
2020-11-13 10:42:39 +0200

    Move loading of date-periods inside resource-opening-hours component

--- src/resource/resource-opening-hours/ResourceOpeningHours.tsx
+++ src/resource/resource-opening-hours/ResourceOpeningHours.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import { IconInfoCircle, Navigation, Button } from 'hds-react';
+import React, { useEffect, useState } from 'react';
+import { IconInfoCircle, Navigation, Button, Notification } from 'hds-react';
+import api, { Resource } from '../../common/utils/api/api';
+import { DatePeriod } from '../../common/lib/types';
 import OpeningPeriod from './opening-period/OpeningPeriod';
 import Collapse from '../../components/collapse/Collapse';
 import './ResourceOpeningHours.scss';
-import { DatePeriod } from '../../common/lib/types';
 
 enum PeriodsListTheme {
   DEFAULT = 'DEFAULT',
@@ -91,24 +92,77 @@ const OpeningPeriodsNotFound = ({ text }: { text: string }): JSX.Element => (
   <p className="opening-periods-not-found">{text}</p>
 );
 
-export default function ResourceOpeningHours({
+const ResourcePeriodsSection = ({
   id,
-  datePeriods,
+  children,
 }: {
   id: string;
-  datePeriods: DatePeriod[];
+  children: React.ReactNode;
+}): JSX.Element => (
+  <Collapse
+    isOpen
+    collapseContentId={`${id}-opening-hours-section`}
+    title="Toimipisteen aukiolotiedot">
+    <p>
+      Toimipisteen aukiolotietoja muokataan jaksokohtaisesti. Aukiolojaksot
+      voivat olla julkaistuja tai julkaisemattomia. Alla voit selata myös
+      tulevia ja menneitä aukiolojaksoja. Näet alla myös eri kieliversiot
+      valitsemalla kielen valikosta. Huomioithan, että palvelu voi itse valita
+      aukiolojaksojen esitystavan, se ei välttämättä ole alla näkyvän kaltainen.
+    </p>
+    {children}
+  </Collapse>
+);
+
+export default function ResourceOpeningHours({
+  id,
+  resource,
+}: {
+  id: string;
+  resource: Resource;
 }): JSX.Element {
-  const [
-    defaultPeriods = [],
-    exceptionPeriods = [],
-  ]: DatePeriod[][] = datePeriods.reduce(
-    ([defaults = [], exceptions = []]: DatePeriod[][], current: DatePeriod) => {
-      return current.override
-        ? [defaults, [...exceptions, current]]
-        : [[...defaults, current], exceptions];
-    },
-    []
-  );
+  const [defaultPeriods, setDefaultPeriods] = useState<DatePeriod[]>([]);
+  const [exceptionPeriods, setExeptionPeriods] = useState<DatePeriod[]>([]);
+  const [hasError, setError] = useState<Error | undefined>(undefined);
+  const [isLoading, setLoading] = useState<boolean>(true);
+
+  useEffect((): void => {
+    // UseEffect's callbacks are synchronous to prevent a race condition.
+    // We can not use an async function as an useEffect's callback because it would return Promise<void>
+    if (resource) {
+      api
+        .getDatePeriod(resource.id)
+        .then((datePeriods: DatePeriod[]) => {
+          setDefaultPeriods(
+            datePeriods.filter((period: DatePeriod) => !period.override)
+          );
+          setExeptionPeriods(
+            datePeriods.filter((period: DatePeriod) => period.override)
+          );
+          setLoading(false);
+        })
+        .catch((e: Error) => {
+          setError(e);
+          setLoading(false);
+        });
+    }
+  }, [resource]);
+
+  if (isLoading) {
+    return (
+      <ResourcePeriodsSection id={id}>
+        <p>Toimipisteen aukiolojaksoja ladataan...</p>
+      </ResourcePeriodsSection>
+    );
+  }
+
+  if (hasError) {
+    return (
+      <ResourcePeriodsSection id={id}>
+        <Notification label="Aukiolojaksoja ei saatu ladattua." type="error" />
+      </ResourcePeriodsSection>
+    );
+  }
 
   return (
     <Collapse
