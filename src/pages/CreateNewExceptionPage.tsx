bfc0fedff9dda996d9a7eb321a6c3039b0972743
Pirkka Huhtala <pirkka.huhtala@kodan.fi>
2022-08-02 17:20:02 +0300

    Add support for saving exception date

--- src/pages/CreateNewExceptionPage.tsx
+++ src/pages/CreateNewExceptionPage.tsx
@@ -1,7 +1,54 @@
+import { DateInput } from 'hds-react';
 import React, { useEffect, useState } from 'react';
-import { Resource, UiDatePeriodConfig, DatePeriod } from '../common/lib/types';
+import { FormProvider, useForm } from 'react-hook-form';
+import { useAppContext } from '../App-context';
+import { formValuesToApiDatePeriod } from '../common/helpers/opening-hours-helpers';
+import {
+  Resource,
+  UiDatePeriodConfig,
+  DatePeriod,
+  Language,
+  OpeningHoursFormValues,
+  ResourceState,
+} from '../common/lib/types';
 import api from '../common/utils/api/api';
+import { formatDate } from '../common/utils/date-time/format';
+import { PrimaryButton, SecondaryButton } from '../components/button/Button';
+import ExceptionForm from '../components/exception-form/ExceptionForm';
+import OpeningHoursTitles from '../components/opening-hours-form/OpeningHoursTitles';
+import ResourceTitle from '../components/resource-title/ResourceTitle';
+import { defaultTimeSpan } from '../constants';
+import useMobile from '../hooks/useMobile';
+import useReturnToResourcePage from '../hooks/useReturnToResourcePage';
 import { getDatePeriodFormConfig } from '../services/datePeriodFormConfig';
+
+const formValuesToException = (
+  resourceIdToSave: number,
+  values: OpeningHoursFormValues
+): DatePeriod => {
+  const data = formValuesToApiDatePeriod(resourceIdToSave, values, values.id);
+  return {
+    ...data,
+    end_date: data.start_date,
+    override: true,
+  };
+};
+
+const getDefaultFormValues = ({
+  date,
+  name,
+}: {
+  date?: string;
+  name?: string;
+}): OpeningHoursFormValues => ({
+  startDate: date ?? formatDate(new Date().toISOString()),
+  endDate: date ?? formatDate(new Date().toISOString()),
+  fixed: true,
+  name: { fi: name ?? '', sv: '', en: '' },
+  override: true,
+  resourceState: ResourceState.CLOSED,
+  openingHours: [],
+});
 
 export default function CreateNewExceptionPage({
   resourceId,
@@ -31,12 +78,94 @@ export default function CreateNewExceptionPage({
     fetchData();
   }, [resourceId]);
 
+  const { language = Language.FI } = useAppContext();
+  const form = useForm<OpeningHoursFormValues>({
+    defaultValues: getDefaultFormValues({}),
+    shouldUnregister: false,
+  });
+  const { register, reset, watch } = form;
+  const startDate = watch('startDate');
+  const isMobile = useMobile();
+  const returnToResourcePage = useReturnToResourcePage();
+  const [isSaving, setSaving] = useState<boolean>(false);
+
   if (!resource || !datePeriodConfig) {
     return <h1>Ladataan...</h1>;
   }
 
-  const submitFn = (data: DatePeriod): Promise<DatePeriod> =>
-    api.postDatePeriod(data);
+  const submitFn = (data: OpeningHoursFormValues): void => {
+    setSaving(true);
+    api
+      .postDatePeriod(formValuesToException(resource.id, data))
+      .then(() => {
+        setSaving(false);
+        returnToResourcePage();
+      })
+      .catch(() => setSaving(false));
+  };
 
-  return <div>Poikkeava päivä</div>;
+  return (
+    <FormProvider {...form}>
+      <form onSubmit={form.handleSubmit(submitFn)}>
+        <div>
+          <ResourceTitle language={language} resource={resource} />
+          <OpeningHoursTitles />
+          <div className="card">
+            <DateInput
+              id="exception-date"
+              data-test="exception-date"
+              ref={register()}
+              disableConfirmation
+              initialMonth={new Date()}
+              label="Poikkeavan päivän päivämäärä"
+              language={language}
+              name="startDate"
+              openButtonAriaLabel="Valitse päivämäärä"
+              value={startDate ?? ''}
+            />
+            <ExceptionForm
+              onClose={(): void =>
+                reset({
+                  openingHours: [],
+                })
+              }
+              onOpen={(): void =>
+                reset({
+                  openingHours: [
+                    {
+                      timeSpanGroups: [
+                        {
+                          timeSpans: [defaultTimeSpan],
+                        },
+                      ],
+                    },
+                  ],
+                })
+              }
+              resourceStates={datePeriodConfig.resourceState.options}
+              isOpen={false}
+              id="exception-form"
+            />
+          </div>
+        </div>
+        <div className="opening-hours-form__actions-container">
+          <div className="card opening-hours-form__actions">
+            <PrimaryButton
+              dataTest="submit-opening-hours-button"
+              isLoading={isSaving}
+              loadingText="Tallentaa aukioloaikoja"
+              type="submit"
+              size={isMobile ? 'small' : 'default'}>
+              Tallenna
+            </PrimaryButton>
+            <SecondaryButton
+              onClick={returnToResourcePage}
+              size={isMobile ? 'small' : 'default'}>
+              Peruuta
+            </SecondaryButton>
+          </div>
+        </div>
+      </form>
+    </FormProvider>
+  );
 }
