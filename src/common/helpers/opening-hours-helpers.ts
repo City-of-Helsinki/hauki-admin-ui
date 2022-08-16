10b60ff727882d2236f0d876bf913ddb72fc22d6
Pirkka Huhtala <pirkka.huhtala@kodan.fi>
2022-08-16 08:15:50 +0300

    Refactor

--- src/common/helpers/opening-hours-helpers.ts
+++ src/common/helpers/opening-hours-helpers.ts
@@ -111,7 +111,6 @@ const toApiTimeSpanGroups = (
     []
   );
 
-// eslint-disable-next-line import/prefer-default-export
 export const datePeriodToApiDatePeriod = (
   resource: number,
   formValues: DatePeriod,
