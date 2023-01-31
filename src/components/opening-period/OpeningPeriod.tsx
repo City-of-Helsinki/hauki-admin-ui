551897faa1da1ffa72159e1a630c62203d92d23e
Pirkka Huhtala <pirkka.huhtala@kodan.fi>
2023-01-31 09:56:48 +0200

    Clean up

--- src/components/opening-period/OpeningPeriod.tsx
+++ src/components/opening-period/OpeningPeriod.tsx
@@ -27,38 +27,34 @@ const OpeningPeriod = ({
   language: Language;
   deletePeriod: (id: number) => Promise<void>;
   initiallyOpen: boolean;
-}): JSX.Element => {
-  const formattedDateRange = formatDateRange({
-    startDate: datePeriod.startDate,
-    endDate: datePeriod.endDate,
-  });
-
-  return (
-    <OpeningPeriodAccordion
-      id={`${datePeriod.id}`}
-      periodName={getDatePeriodName(language, datePeriod)}
-      dateRange={formattedDateRange}
-      onDelete={(): Promise<void> => {
-        if (datePeriod.id) {
-          return deletePeriod(datePeriod.id);
-        }
-        return Promise.resolve();
-      }}
-      editUrl={editUrl}
-      initiallyOpen={initiallyOpen}
-      isActive={current}>
-      <div className="date-period-details-container">
-        {datePeriod.resourceState === ResourceState.CLOSED ? (
-          'Suljettu'
-        ) : (
-          <OpeningHoursPreview
-            openingHours={datePeriod.openingHours}
-            resourceStates={datePeriodConfig?.resourceState.options ?? []}
-          />
-        )}
-      </div>
-    </OpeningPeriodAccordion>
-  );
-};
+}): JSX.Element => (
+  <OpeningPeriodAccordion
+    id={`${datePeriod.id}`}
+    periodName={getDatePeriodName(language, datePeriod)}
+    dateRange={formatDateRange({
+      startDate: datePeriod.startDate,
+      endDate: datePeriod.endDate,
+    })}
+    onDelete={(): Promise<void> => {
+      if (datePeriod.id) {
+        return deletePeriod(datePeriod.id);
+      }
+      return Promise.resolve();
+    }}
+    editUrl={editUrl}
+    initiallyOpen={initiallyOpen}
+    isActive={current}>
+    <div className="date-period-details-container">
+      {datePeriod.resourceState === ResourceState.CLOSED ? (
+        'Suljettu'
+      ) : (
+        <OpeningHoursPreview
+          openingHours={datePeriod.openingHours}
+          resourceStates={datePeriodConfig?.resourceState.options ?? []}
+        />
+      )}
+    </div>
+  </OpeningPeriodAccordion>
+);
 
 export default OpeningPeriod;
