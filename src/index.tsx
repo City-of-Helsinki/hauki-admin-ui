161cb7f6e8a0e210a6c83aacd9168685d6139187
Mikko Riippi <mikko.riippi@gofore.com>
2026-04-02 13:39:55 +0300

    fix: remove CRA service worker
    Refs: KEH-281

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
- 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
@@ -6,7 +5,6 @@ import * as Sentry from '@sentry/react';
 import './index.scss';
 import axe from '@axe-core/react';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 declare global {
   interface Window {
@@ -45,8 +43,3 @@ if (!container) {
 }
 const root = createRoot(container);
 root.render(<App />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
